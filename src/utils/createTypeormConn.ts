221b63fe53ffd6ae41bb8738558c9fd1a6ead14c
Adisak Chaiyakul <adiaskchaiyakul@gmail.com>
2019-05-17 05:53:15 +0700

    register more type

--- src/utils/createTypeormConn.ts
+++ src/utils/createTypeormConn.ts
@@ -6,6 +6,8 @@ import { LineUser } from "../entity/LineUser";
 import { Transection } from "../entity/Transection";
 import { User } from "../entity/User";
 import { Writer } from "../entity/Writer";
+import { Staff } from "../entity/Staff";
+import { Invite } from "../entity/Invite";
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV === 'production' ? 'production' : 'default');
@@ -14,7 +16,7 @@ export const createTypeormConn = async () => {
     ? createConnection({
       ...connectionOptions,
       url: process.env.DATABASE_URL,
-      entities: [Action, Book, Category, LineUser, Transection, User, Writer],
+      entities: [Action, Book, Category, LineUser, Transection, User, Writer, Staff, Invite],
       name: "default"
     } as any)
     : createConnection({ ...connectionOptions, name: "default" });
